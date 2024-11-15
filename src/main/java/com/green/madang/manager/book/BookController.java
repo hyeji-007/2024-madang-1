1f68743381ab972377d1a6577a12b8a5a67cadd4
KwonHyeji <kwonhyeji007@gmail.com>
2024-11-15 18:02:56 +0900

    11/15 spring class

---
+++ src/main/java/com/green/madang/manager/book/BookController.java
@@ -0,0 +1,48 @@
+package com.green.madang.manager.book;
+
+import com.green.madang.common.model.MyResponse;
+import com.green.madang.manager.book.model.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor //final이 붙은 멤버필드의 생성자를 만들어준다.
+@RestController //데이터로 응답하는 것, 요청 받으면 응답하는 역할
+@RequestMapping("manager/book")
+@Tag(name = "도서", description = "도서 API")
+public class BookController {
+    private final BookService service;
+
+    @PostMapping
+    @Operation(summary = "도서 입고", description = "도서 입고 처리 API")
+    public MyResponse<Integer> postBook(@RequestBody BookPostReq p) {
+        int result = service.postBook(p);
+        MyResponse<Integer> response = new MyResponse<>("책 등록 완료", result);
+        return response;
+    }
+
+    @GetMapping
+    @Operation(summary = "도서 리스트", description =  "도서 정보 리스트 보기 API")
+    public MyResponse<List<BookGetRes>> getBookList(@ParameterObject @ModelAttribute BookGetReq p) {
+        List<BookGetRes> list = service.getBookList(p);
+        return new MyResponse<>(p.getPage() + "페이지 데이터", list);
+    }
+
+    @PutMapping
+    @Operation(summary = "도서 수정")
+    public MyResponse<Integer> putBook(@RequestBody BookPutReq p) {
+        return new MyResponse<>(p.getBookId() + "번 도서 수정"
+                , service.putBook(p));
+    }
+
+    @DeleteMapping
+    @Operation(summary = "도서 삭제")
+    public MyResponse<Integer> deleteBook(@ParameterObject @ModelAttribute BookDeleteReq p) {
+        return new MyResponse<>(p.getBookId() + "번 도서 삭제"
+                , service.deleteBook(p));
+    }
+}
---
+++ src/main/java/com/green/madang/manager/book/model/BookGetReq.java
@@ -0,0 +1,15 @@
+package com.green.madang.manager.book.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookGetReq {
+    private int page; //실제 받는 값
+    private int size; //실제 받는 값
+    private String searchText;
+    @JsonIgnore //Swagger 문서에서 멤버필드가 안 나왔으면 할 때 이 애노테이션을 붙이면 된다.
+    private int startIdx; //(page - 1) * size
+}
