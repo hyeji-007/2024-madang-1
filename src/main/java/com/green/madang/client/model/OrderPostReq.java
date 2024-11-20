cc00aa613d8906524549ad0631791eb1313ee809
KwonHyeji <kwonhyeji007@gmail.com>
2024-11-20 10:55:00 +0900

    11/20 spring class

--- src/main/java/com/green/madang/client/model/OrderPostReq.java
+++ src/main/java/com/green/madang/client/model/OrderPostReq.java
@@ -3,9 +3,11 @@ package com.green.madang.client.model;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class OrderPostReq {
     private int custId;
     private int bookId;
